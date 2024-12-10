e233d77a692ece608dbf43fe251ccb9b9641ac60
bgidd001 <none@none.none>
2024-12-10 12:46:25 -0500

    finals

--- game/locations/B_G.py
+++ game/locations/B_G.py
@@ -5,10 +5,12 @@ from game.events import *
 import game.items as items
 import game.combat as combat
 import game.event as event
-import game.items as item
+from game.items import Item
 import random
 from game.context import Context
 from game.combat import Monster
+from game.display import menu
+
 
 
 class IslandB(location.Location):
@@ -53,7 +55,7 @@ class Dungeon_map(items.Item):
 
     def process_verb (self, verb, cmd_list, nouns):
         if (verb == "read"):
-         display.announce("Study the scroll, it reveals the path for the dungeon! N:E:N")
+         display.announce("Study the scroll, it reveals the path for the dungeon! W:N:E:E:N")
 
 class Damp_cavern (location.SubLocation):
     def __init__ (self, m):
@@ -74,7 +76,7 @@ class Damp_cavern (location.SubLocation):
 
         if (verb == "west"):
             display.announce("You ascend deeper into the cavern, and discover a room.")
-            config.the_player.next_loc = self.main_location.locations["room"]
+            config.the_player.next_loc = self.main_location.locations["door"]
             config.the_player.go = True
 
         if (verb == "east"):
@@ -119,13 +121,89 @@ class Damp_cavern (location.SubLocation):
     def find_adventurer(self):
          display.announce("As the rats disperse from around you, a forsaken body appears.")
          display.announce("You notice a scroll lodged in their tattered clothing.")
-         display.announce("Study the scroll, it reveals the path for the dungeon! W:N:E:N")
+         display.announce("Study the scroll, it reveals the path for the dungeon! W:N:E:E:N")
          #adds to inventory
          config.the_player.add_to_inventory([Dungeon_map()])
 
 
+class Ornate_door(location.SubLocation):
+    def __init__ (self, m):
+        super().__init__(m)
+        self.name = "door"
+        self.verbs['north'] = self
+        self.verbs['south'] = self
+        self.verbs['east'] = self
+        self.verbs['west'] = self
 
-class Entry_room(location.SubLocation):#West to get here
+        self.event_chance = 100
+        self.events.append(OoozeEvent())
+
+    def enter (self):
+        description = "You enter a room with a very ornate door to the north."
+        display.announce(description)
+
+    def process_verb (self, verb, cmd_list, nouns):
+        if (verb == "south"):
+            config.the_player.next_loc = self.main_location.locations["cavern"]
+            config.the_player.go = True
+        if (verb == "north"):
+            config.the_player.next_loc = self.main_location.locations["room"]
+            config.the_player.go = True
+
+class Ooze(Monster):
+    def __init__ (self):
+        attacks = {}
+        attacks["splash"] = ["splashes",random.randrange(55,110), (10,15)]
+        attacks["spit"] = ["spit",random.randrange(70,90), (5,15)]
+        super().__init__("Ooze", random.randint(50,100), attacks, 40 + random.randint(0, 10))
+        self.type_name = "Ooze" 
+class OoozeEvent(event.Event):
+
+    def __init__ (self):
+        self.name = " Oooze attacks."
+
+    def process (self, world):
+        result = {}
+        Oooze = Oooze()
+        display.announce("A giant Ooze drops from the ceiling")
+        combat.Combat([Oooze]).combat()
+        display.announce("The Oooze disintegrates into the air.")
+        result["newevents"] = []
+        result["message"] = ""
+        display.announce("A mysterious weapon appears to fall from the Oozes body.")
+        config.the_player.add_to_inventory([Oozeblaster()])
+        return result
+class Oozeblaster(Item):
+
+    # triple shot oozeblaster.
+    def __init__(self):
+        super().__init__("oozeblaster", 10)
+        self.damage = (25,75)
+        self.firearm = True
+        self.charges = 3
+        self.skill = "guns"
+        self.verb = "shoot"
+        self.verb2 = "shoots"
+        self.NUMBER_OF_ATTACKS = 3 # Number of attacks to be made in pickTargets
+
+    def pickTargets(self, action, attacker, allies, enemies):
+        if (len(enemies) <= self.NUMBER_OF_ATTACKS): # If less than or equal to three targets, hit everyone
+            return enemies
+        else:
+            options = []
+            for t in enemies:
+                options.append("attack " + t.name)
+            targets = []
+
+            while(len(targets) < self.NUMBER_OF_ATTACKS): # While loop so that it keeps going until the player picks three different targets.
+                display.announce(f"Pick target number {len(targets)}.", pause=False)
+                choice = menu(options)
+                if(not choice in targets):
+                    targets.append(enemies[choice])
+            return targets
+
+
+class Entry_room(location.SubLocation):
     def __init__ (self, m):
         super().__init__(m)
         self.name = "room"
@@ -144,8 +222,8 @@ class Entry_room(location.SubLocation):#West to get here
         display.announce(description)
 
     def process_verb (self, verb, cmd_list, nouns):
-        if (verb == "exit" or verb == "leave"):
-            config.the_player.next_loc = self.main_location.locations["cavern"]
+        if (verb == "south" or verb == "leave"):
+            config.the_player.next_loc = self.main_location.locations["door"]
             config.the_player.go = True
         if (verb == "north"):
             config.the_player.next_loc = self.main_location.locations["armorroom"]
@@ -173,8 +251,8 @@ class SkeletonEvent(event.Event):
         result["message"] = ""
         return result
     
-class Armor_room(location.SubLocation): #North to get here
-    def __init__ (self, m):
+class ArmorRoom:
+    def __init__(self, m):
         super().__init__(m)
         self.name = "armorroom"
         self.verbs['north'] = self
@@ -183,31 +261,99 @@ class Armor_room(location.SubLocation): #North to get here
         self.verbs['west'] = self
         self.verbs['pickup'] = self
         self.verbs['place'] = self
-        self.pickedup = []
+        self.pickedup = []  # Items the player has picked up
         self.floor_items = ["bascinet", "byrnie", "chausses", "aegis", "strop", "canthus", "rapier", "katana"]
-        self.mannequin_items = []
-        self.required_items = ["bascinet", "byrnie", "chausses", "rapier"]
-    #list of words picked up, on the floor, on the mannequin. where the value changes. description is based on the state of the lists
-    
+        self.mannequin_items = []  # The mannequin is initially empty
+        self.required_items = ["bascinet", "byrnie", "chausses"]  # Items needed to complete the mannequin
 
-    def enter (self):   #pickup the armor pieces scattered amongst garbage
-        display.announce("Upon entry you notice scattered pieces of armor, 2 statues and one of the two are un-armored.")
-    #
-    def process_verb (self, verb, cmd_list, nouns):
-        if (verb == "south"):
+    def enter(self):
+        # Generate descriptions for the floor items and mannequin state
+        if len(self.floor_items) > 0:
+            floor_description = f"Upon entry, you notice scattered pieces of armor: {', '.join(self.floor_items)}."
+        else:
+            floor_description = "The floor is empty, void of any armor or equipment."
+
+        if len(self.mannequin_items) == 0:
+            mannequin_description = "The mannequin is completely stripped of armor."
+        else:
+            mannequin_description = f"The mannequin is armored with the following pieces: {', '.join(self.mannequin_items)}."
+
+        # Check if the mannequin is complete with the required items
+        if all(item in self.mannequin_items for item in self.required_items):
+            complete_message = "the mannequins look similar!"
+        else:
+            complete_message = "The mannequin is still incomplete."
+
+        # Combine the descriptions and announce
+        description = f"{floor_description} {mannequin_description} {complete_message}"
+        display.announce(description)
+
+    def process_verb(self, verb, cmd_list, nouns):
+        if verb == "south":
             config.the_player.next_loc = self.main_location.locations["room"]
-        if (verb == "north" or verb == "east" or verb == "west"):
+            config.the_player.go = True
+
+        elif verb in ["north", "west"]:
+            config.the_player.next_loc = self.main_location.locations["armorroom"]
+        elif verb == "east":
             config.the_player.next_loc = self.main_location.locations["door"]
-        if verb == "pickup":
-            self.pickedup.append()
+            config.the_player.go = True
+
+        elif verb == "pickup":
+            if nouns and nouns[0] in self.floor_items:
+                item = nouns[0]
+                self.floor_items.remove(item)
+                self.pickedup.append(item)
+                display.announce(f"You picked up the {item}.")
+            elif nouns:
+                display.announce(f"There is no {nouns[0]} on the floor.")
+            else:
+                display.announce("What item do you want to pick up?")
+        elif verb == "place":
+            if nouns and nouns[0] in self.pickedup:
+                item = nouns[0]
+                # Place the item on the mannequin
+                self.pickedup.remove(item)
+                self.mannequin_items.append(item)
+                display.announce(f"You placed the {item} on the mannequin.")
+                
+                # Check if the mannequin is complete with the required items
+                if all(required_item in self.mannequin_items for required_item in self.required_items):
+                    display.announce("The mannequin is now complete with all required armor pieces!")
+            else:
+                display.announce("You don't have that item to place or the item isn't in your inventory.")
 
 
-        #add a comment for when they complete the puzzle(assembling the armor on the statuette, a unlocking sound can be heard to the east.
+        
 
-#class next_loct #east to get here
-class Ornate_door(location.SubLocation):
-    pass
 
 #class next_loct #north to get here #back to the beach
+class Treasure_room (location.SubLocation):
+    def __init__ (self, m):
+        super().__init__(m)
+        self.name = "Treasure"
+        self.verbs['exit'] = self
+        self.verbs['leave'] = self
+        self.verbs['north'] = self
+        self.verbs['south'] = self
+        self.verbs['east'] = self
+        self.verbs['west'] = self
+        self.verbs["open"] = self
+    
+    def enter (self):
+        display.announce ("You assemble the mannequin, the door opens to reveal a golden chest.")
 
+    def process_verb (self, verb, cmd_list, nouns):
+        if (verb == "south", verb == "east"):
+            display.announce ("You find yourself looking at a wall.", pause=False)
+        if (verb in ["north", "exit", "leave"]):
+            display.announce ("You return to your ship.", pause=False)
+            config.the_player.next_loc = self.main_location.locations["rockybeach"]
+            self.main_location.end_visit()
 
+        if (verb == "open"):
+            display.announce ("You open the chest, and feel your body recovering as if magic!")
+            for i in config.the_player.get_pirates():
+                i.lucky = True
+                i.sick = False
+                i.health = i.max_health
